3fc110f94e6bceb2396429fccbdb9f060f71e978
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-04 17:22:14 -0200

    potter: LineageActions -> MotoActions

--- device.mk
+++ device.mk
@@ -189,9 +189,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8953
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Media
 PRODUCT_PACKAGES += \
