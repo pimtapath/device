406dac5833d7fe66849f74f62f0ec836d59a88e3
fab <f4bi1@free.fr>
2018-09-10 13:39:44 +0000

    typo

--- device.mk
+++ device.mk
@@ -320,4 +320,3 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/carbon/config/common.mk)
 $(call inherit-product, vendor/carbon/config/gsm.mk)
-
