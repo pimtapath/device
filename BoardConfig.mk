052f2352b3a7f50dbd0b6e58eeb3ecc652a2d552
Alberto97 <albertop2197@gmail.com>
2018-11-10 21:39:13 +0100

    Pin libmot_gpu_mapper and kill its shim

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,6 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so \
     /system/vendor/lib64/libsensorndkbridge.so|libshim_ril.so \
-    /system/vendor/lib/libmot_gpu_mapper.so|libgpu_mapper_shim.so \
     /system/lib/libjustshoot.so|libjustshoot_shim.so \
     /system/vendor/lib64/vendor.qti.gnss@1.0_vendor.so|libgnss_shim.so \
     /system/lib64/vendor.qti.gnss@1.0.so|libgnss_shim.so
