65f751c5020be45fe204c0d674e1ace8fbad4857
bzhpowa <f4bi1@free.fr>
2018-12-15 09:55:01 +0100

    cpu governor back, fix sedenial

--- rootdir/etc/cpugovernor.sh
+++ rootdir/etc/cpugovernor.sh
@@ -1,8 +1,5 @@
 #!/vendor/bin/sh
 
-echo 'cfq' > /sys/block/mmcblk0/queue/scheduler
-echo '1792' > /sys/block/mmcblk0/queue/read_ahead_kb
-
 if [ -d /sdcard/electron ]; then
     echo 'electron' > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 else
